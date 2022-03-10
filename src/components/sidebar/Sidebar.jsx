083b49c5dfd504b8477fe8ac6755b0d60d6289dc
ciccone1978 <ciccone1978@gmail.com>
2022-03-10 21:52:58 +0100

    Sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,45 @@
 import "./sidebar.css"
+import { RssFeed, Chat, PlayCircleFilled, Group } from "@material-ui/icons"
 
 export default function Sidebar(){
     return(
         <div className="sidebar">
-            sidebar
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeed className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Feeds</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Chat className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Chats</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <PlayCircleFilled className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Videos</span>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Group className="sidebarIcon"/>
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+                </ul>
+                <button className="sidebarButton">Show More</button>
+                <hr className="sidebarHr" />
+                <ul className="sidebarFriendList">
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span>
+                    </li>
+                    <li className="sidebarFriend">
+                        <img src="/assets/person/2.jpeg" alt="" className="sidebarFriendImg" />
+                        <span className="sidebarFriendName">Jane Doe</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
